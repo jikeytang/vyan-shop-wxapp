7b07ad5697cd13850b0756d81b4c0ec477b5934a
jikeytang <jikeytang@gmail.com>
2019-06-30 23:13:04 +0800

    删除无用代码

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # vyan-shop-wxapp
 微燕商城-微信小程序
 
-[微燕商城 - 微信公众号](https://github.com/jikeytang/vyan-shop-weixin)  
-[微燕商城 - 微信小程序](https://github.com/jikeytang/vyan-shop-wxapp)  
-[微燕商城 - admin](https://github.com/jikeytang/vyan-shop-admin)  
-[微燕商城 - node](https://github.com/jikeytang/vyan-shop-node)  
+- [微燕商城 - 微信公众号](https://github.com/jikeytang/vyan-shop-weixin)  
+- [微燕商城 - 微信小程序](https://github.com/jikeytang/vyan-shop-wxapp)  
+- [微燕商城 - admin](https://github.com/jikeytang/vyan-shop-admin)  
+- [微燕商城 - node](https://github.com/jikeytang/vyan-shop-node)  
--- pages/index/index.js
+++ pages/index/index.js
@@ -18,21 +18,6 @@ Page({
     interval: 5000,
     duration: 1000
   },
-  changeIndicatorDots: function(e) {
-    this.setData({
-      indicatorDots: !this.data.indicatorDots
-    })
-  },
-  changeAutoplay: function(e) {
-    this.setData({
-      autoplay: !this.data.autoplay
-    })
-  },
-  intervalChange: function(e) {
-    this.setData({
-      interval: e.detail.value
-    })
-  },
   durationChange: function(e) {
     this.setData({
       duration: e.detail.value
@@ -45,39 +30,5 @@ Page({
     })
   },
   onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
-  },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
   }
 })
